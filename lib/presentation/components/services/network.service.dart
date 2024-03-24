1d56b0830085f62208b92cf5895e2495141e3364
soebedjho <gerosellysas@gmail.com>
2024-03-24 13:16:13 +0700

    create: network service for rest_api network repository methods

---
+++ lib/presentation/components/services/network.service.dart
@@ -0,0 +1,52 @@
+import 'package:get/get.dart';
+import 'package:wander_ways/features/network/data/network.data.dart';
+import 'package:wander_ways/features/network/domain/network.domain.dart';
+
+class NetworkService extends GetxService {
+  final RestApiRepo _restApiRepo = RestApiRepo();
+
+  NetworkGetAllUseCase _getAllData(IRestApi repo) => NetworkGetAllUseCase(
+        repo: repo,
+      );
+
+  NetworkGetSingleUseCase _getSingleData(IRestApi repo) =>
+      NetworkGetSingleUseCase(
+        repo: repo,
+      );
+
+  NetworkPutUseCase _putData(IRestApi repo) => NetworkPutUseCase(
+        repo: repo,
+      );
+
+  var listTrip = <Trip>[].obs;
+  var trip = const Trip().obs;
+
+  Future<List<Trip>> _getAllTripData() async {
+    return await _getAllData(_restApiRepo).invoke() as List<Trip>;
+  }
+
+  Future<List<Trip>> getAllSelectedTripData(Trip trip) async {
+    var res = await _getAllTripData();
+    var tripList = <Trip>[];
+
+    for (var r in res) {
+      if (r.origin == trip.origin && r.destination == r.destination) {
+        tripList.add(r);
+      }
+    }
+
+    return tripList;
+  }
+
+  Future<Trip> getSingleTripData(int tid) async {
+    return await _getSingleData(_restApiRepo).invoke(params: {
+      "tid": tid,
+    }) as Trip;
+  }
+
+  Future<Trip> updateTripSeat(Trip trip) async {
+    return await _putData(_restApiRepo).invoke(params: {
+      "trip": trip,
+    }) as Trip;
+  }
+}
